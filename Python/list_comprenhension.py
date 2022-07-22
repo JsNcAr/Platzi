0021bf136cf7e2f90bbc7e766d64a5c9475282fc
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-07-22 18:15:14 -0500

    Added challenge of list comprenhension class

--- Python/list_comprenhension.py
+++ Python/list_comprenhension.py
@@ -1,7 +1,9 @@
 def main():
     squared_not_module_3 = [num ** 2 for num in range(1, 101) if num % 3 != 0]
+    multiple = [num*4 for num in range(1, int(100000/4)+1) if num*4 % 18 == 0]
 
-    print(squared_not_module_3)
+    print(multiple)
+
 
 if __name__ == '__main__':
     main()
