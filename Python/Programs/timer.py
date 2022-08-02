6086588ed0ea41971ac101f6ff95814f46cef486
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-08-02 15:33:41 -0500

    updated timer.py to work with any given arguments

--- Python/Programs/timer.py
+++ Python/Programs/timer.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         initial_time = datetime.now()
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
         print(f'Execution time: {(final_time - initial_time).total_seconds()} seconds')
     return wrapper
