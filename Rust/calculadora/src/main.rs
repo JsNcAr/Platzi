faedcbabc2bf0ace93b14366f27794a92bc707d3
JsNcAr <jn.arias@uniandes.edu.co>
2024-05-06 22:08:18 -0500

    Refactor code to avoid code repetition

--- Rust/calculadora/src/main.rs
+++ Rust/calculadora/src/main.rs
@@ -2,13 +2,6 @@ use regex::Regex;
 use std::io;
 
 fn main() {
-    let re_add = Regex::new(r"(-?\d+)\s*\+\s*(-?\d+)").unwrap();
-    let re_subs = Regex::new(r"(-?\d+)\s*\-\s*(-?\d+)").unwrap();
-    let re_mult = Regex::new(r"(-?\d+)\s*\*\s*(-?\d+)").unwrap();
-    let re_div = Regex::new(r"(-?\d+)\s*\/\s*(-?\d+)").unwrap();
-    // Regex to check parentheses
-    let re_parentheses = Regex::new(r"\(([^()]*)\)").unwrap();
-
     // Get user data
     println!("Enter an operation:");
     let mut input = String::new();
@@ -19,19 +12,32 @@ fn main() {
         .expect("Failed to read line");
 
     // Make operation in parentheses
+    let mut new_input = solve_parentesis(input.clone());
+
+    loop {
+        let (operation_name, re) = get_operation(&new_input);
+        if operation_name == "none" {
+            break;
+        }
+        let result = apply_operation(&new_input, operation_name, re);
+        new_input = result.to_string();
+    }
+
+    println!("Result: {}", new_input);
+}
+
+fn solve_parentesis(input: String) -> String {
+    let re_parentheses = Regex::new(r"\(([^()]*)\)").unwrap();
     let mut new_input: String = input.clone();
-    let mut operation: String = String::new();
 
     if re_parentheses.is_match(&input) {
-        println!("Input: {}", input);
         for caps in re_parentheses.captures_iter(&input) {
-            operation = caps[1].to_string();
+            let mut operation = caps[1].to_string();
             loop {
                 let (operation_name, re) = get_operation(&operation);
                 if operation_name == "none" {
                     // Replace specific parentheses
                     new_input = re_parentheses.replace(&new_input, &operation).to_string();
-                    println!("Parenthesis: {}", new_input);
                     break;
                 }
                 let result = apply_operation(&operation, operation_name, re);
@@ -39,41 +45,7 @@ fn main() {
             }
         }
     }
-    if re_mult.is_match(&input) {
-        loop {
-            let (operation_name, re) = ("mult", re_mult.clone());
-            let result = apply_operation(&new_input, operation_name, re);
-            new_input = result.to_string();
-            if !re_mult.is_match(&new_input) {
-                println!("Mult: {}", new_input);
-                break;
-            }
-        }
-    }
-    if re_div.is_match(&input) {
-        loop {
-            let (operation_name, re) = ("div", re_div.clone());
-            let result = apply_operation(&new_input, operation_name, re);
-            new_input = result.to_string();
-            if !re_div.is_match(&new_input) {
-                println!("Div: {}", new_input);
-                break;
-            }
-        }
-    }
-    if re_add.is_match(&input) || re_subs.is_match(&input) {
-        loop {
-            let (operation_name, re) = get_operation(&new_input);
-            if operation_name == "none" {
-                break;
-            }
-            let result = apply_operation(&new_input, operation_name, re);
-            println!("Result: {}", result);
-            new_input = result.to_string();
-        }
-    }
-
-    println!("Result: {}", new_input);
+    return new_input;
 }
 
 fn get_operation(input: &str) -> (&str, Regex) {
